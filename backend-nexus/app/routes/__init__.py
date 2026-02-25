ff6ff1607faaa8dbc28d18b29d018dda41daa2be
Dite Qela <149471713+diteeee@users.noreply.github.com>
2026-02-25 23:50:20 +0100

    user_tenant_plan fix

--- backend-nexus/app/routes/__init__.py
+++ backend-nexus/app/routes/__init__.py
@@ -3,7 +3,5 @@ from .superadmin_tenant import router as superadmin_tenant_router
 from .public_tenant import router as public_tenant_router
 from .tenant_audit_log import router as tenant_audit_log
 from .user_tenant_plan import router as user_tenant_plan_router
-from .user import router as user_router
-from .tenant_manager import router as tenant_manager_router
 
-__all__ = ["role_router", "superadmin_tenant_router", "public_tenant_router", 'tenant_audit_log', 'user_tenant_plan_router', 'user_router', 'tenant_manager_router']
+__all__ = ["role_router", "superadmin_tenant_router", "public_tenant_router", 'tenant_audit_log', 'user_tenant_plan_router']
